6be7aa0906a298adb502ec8674953160308f432b
Guilherme <guilhaugusto05@gmail.com>
2022-01-13 15:49:15 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from './trivia.png';
+// import logo from './trivia.png';
+import Routes from './components/Routes';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Routes />
+    );
+  }
 }
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Login from './Login';
+import Settings from './Settings';
+import Game from './Game';
+import Ranking from './Ranking';
+import Feedback from './Feedback';
+
+export { Login, Settings, Game, Ranking, Feedback };
